503f41003be1b4228bf1410dcbed852df2481f8e
yadavkuldeepttt <yadavkuldeepdell@gmail.com>
2025-02-05 10:23:58 +0530

    fixed issue

--- src/components/layout/home/heroSection.jsx
+++ src/components/layout/home/heroSection.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/Button';
 
 const HeroSection = () => {
   const [isHovered, setIsHovered] = useState(false);
